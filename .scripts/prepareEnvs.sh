ef4ea07caf0241c2f39d10bd95d3d7b9bc22931c
Hyunwoo Kim <admin@hyunwoo.kim>
2023-02-21 23:03:36 +0900

    Remove examples/.env print from prepareEnvs.sh

--- .scripts/prepareEnvs.sh
+++ .scripts/prepareEnvs.sh
@@ -2,7 +2,6 @@ echo "prepareEnvs.sh $PWD"
 
 if [ $THECAMP_ID ]; then
   echo "THECAMP_ID=$THECAMP_ID\nTHECAMP_PW=$THECAMP_PW" >> .env
-  echo "THECAMP_ID=$THECAMP_ID\nTHECAMP_PW=$THECAMP_PW" >> examples/.env
 
   if [ -d "dist" ]; then
     echo "THECAMP_ID=$THECAMP_ID\nTHECAMP_PW=$THECAMP_PW" >> dist/.env
